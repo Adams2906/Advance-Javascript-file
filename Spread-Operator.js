1226467856fa9a8e3fb03675ee7d5ace12e69e8e
Joshua Solomon Adams <joshuaadams2906@gmail.com>
2024-06-27 10:53:13 +0100

    New commit

--- Spread-Operator.js
+++ Spread-Operator.js
@@ -34,3 +34,25 @@ Given a string, it's easy to spread it out into separate array items:
 const greeting = "Hello";
 const arrayOfChars = [...greeting];
 console.log(arrayOfChars); //  ['H', 'e', 'l', 'l', 'o']
+
+/*
+Copy either an object or an array into a separate one
+Here's how to copy an object into a completely separate object, using the spread operator.
+*/
+
+const car1 = {
+    speed: 200,
+    color: 'yellow'
+}
+const car2 = {...car1}
+
+car1.speed = 201
+
+console.log(car1.speed, car2.speed);
+
+//You can copy an array into a completely separate array, also using the spread operator, like this:
+
+const fruits1 = ['apples', 'pears']
+const fruits2 = [...fruits1]
+fruits1.pop()
+console.log(fruits1, "not", fruits2);
